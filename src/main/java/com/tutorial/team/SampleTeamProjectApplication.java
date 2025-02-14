98ad82f8d61e8c1200a9bff2d3e1e9da72f7ab5b
choi-ys <project.log.062@gmail.com>
2025-02-14 15:18:56 +0900

    initial commit

---
+++ src/main/java/com/tutorial/team/SampleTeamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleTeamProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleTeamProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorial/team/SampleTeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleTeamProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
